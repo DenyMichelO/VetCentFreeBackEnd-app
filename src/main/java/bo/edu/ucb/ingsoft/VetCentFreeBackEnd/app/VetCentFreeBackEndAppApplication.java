aac7387e16dd9978c31fe931ab99e81e78494d40
DenyMichelO <dennys.michel@ucb.edu.bo>
2021-06-08 22:54:43 -0400

    Subiendo primera version del proyecto

---
+++ src/main/java/bo/edu/ucb/ingsoft/VetCentFreeBackEnd/app/VetCentFreeBackEndAppApplication.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.ingsoft.VetCentFreeBackEnd.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetCentFreeBackEndAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetCentFreeBackEndAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bo/edu/ucb/ingsoft/VetCentFreeBackEnd/app/VetCentFreeBackEndAppApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.ingsoft.VetCentFreeBackEnd.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetCentFreeBackEndAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
